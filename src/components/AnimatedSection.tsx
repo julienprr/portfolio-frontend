e39daf34c3b6f71225577e63180fde536f4d7202
Julien Perrier <perrierjulien22@gmail.com>
2025-07-11 15:52:52 +0200

    fix build

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -1,36 +1,8 @@
 import React from 'react';
 
-import { motion } from 'framer-motion';
-
 import FadeInOnScroll from './FadeInOnScroll';
 
-const containerVariants = {
-  hidden: {},
-  visible: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const childVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.5 },
-  },
-};
-
-const AnimatedSection = ({
-  className,
-  children,
-  stagger = true,
-}: {
-  className?: string;
-  children: React.ReactNode;
-  stagger?: boolean;
-}) => (
+const AnimatedSection = ({ children }: { className?: string; children: React.ReactNode; stagger?: boolean }) => (
   <section>
     {React.Children.map(children, (child) => (
       <FadeInOnScroll>{child}</FadeInOnScroll>
